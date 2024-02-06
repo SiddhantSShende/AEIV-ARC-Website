c7f2d3f05721d69705bc64e8975526b11ebb191a
Sid <siddhantsshende@gamil.com>
2024-02-06 10:13:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+<script type="module" src="https://unpkg.com/@splinetool/viewer@1.0.34/build/spline-viewer.js">
+    import { Application } from '@splinetool/runtime';
+
+    const canvas = document.getElementById('canvas3d');
+    const app = new Application(canvas);
+
+
+
+</script>
